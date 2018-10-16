0c90d78df542cc8b2e209b5572179b744560d75f
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-10-16 16:35:02 -0400

    adds answer button

--- cards-against-humanity/scripts/index.js
+++ cards-against-humanity/scripts/index.js
@@ -2,13 +2,15 @@
 const triggerElement = document.querySelector('[data-trigger]');
 const outputElement = document.querySelector('[data-output]');
 
+
+
 // function that fetches card
-function getCard () {
+function getCardQuestion () {
     fetch('http://my-little-cors-proxy.herokuapp.com/https://cardsagainsthumanity-api.herokuapp.com/pick')
         .then(r => r.json())
         // .then(convertToJson)
     //    .then(data => data)
-       .then(extractCard)
+       .then(extractQuestion)
        .then(drawCard)
 }
 
@@ -16,7 +18,7 @@ function getCard () {
 //     return r.json();
 // }
 
-function extractCard(cardObj) {
+function extractQuestion(cardObj) {
     // console.log(cardObj)
     return cardObj.question;
     console.log(cardObj.answer)
@@ -37,7 +39,51 @@ function drawCard(cardText) {
 
 // main function that attaches button listener
 function main() {
-    triggerElement.addEventListener('click', getCard);
+    triggerElement.addEventListener('click', getCardQuestion);
 }
 
 main();
+
+// second button
+const triggerElement2 = document.querySelector('[data-trigger2]');
+const outputElement2 = document.querySelector('[data-output2]');
+
+// function that fetches card
+function getCardAnswer () {
+    fetch('http://my-little-cors-proxy.herokuapp.com/https://cardsagainsthumanity-api.herokuapp.com/pick')
+        .then(r => r.json())
+        // .then(convertToJson)
+    //    .then(data => data)
+       .then(extractAnswer)
+       .then(drawCard)
+}
+
+// function convertToJson(r) {
+//     return r.json();
+// }
+
+function extractAnswer(cardObj) {
+    // console.log(cardObj)
+    // return cardObj.question;
+    return cardObj.answer
+}
+
+// function that draws card to DOM
+
+function drawCard(cardText) {
+    const newCard = document.createElement('div');
+    newCard.textContent = cardText;
+
+    // append
+    outputElement2.appendChild(newCard);
+}
+
+
+
+
+// main function that attaches button listener
+function main2() {
+    triggerElement2.addEventListener('click', getCardAnswer);
+}
+
+main2();
