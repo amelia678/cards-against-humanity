c0d06d6c96f3b1e969e64ed76c40df5d0204c4a6
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-10-16 15:55:56 -0400

    added functionalilty to append question to screen

--- cards-against-humanity/scripts/index.js
+++ cards-against-humanity/scripts/index.js
@@ -1,3 +1,43 @@
 // dom selection
+const triggerElement = document.querySelector('[data-trigger]');
+const outputElement = document.querySelector('[data-output]');
 
 // function that fetches card
+function getCard () {
+    fetch('http://my-little-cors-proxy.herokuapp.com/https://cardsagainsthumanity-api.herokuapp.com/pick')
+        .then(r => r.json())
+        // .then(convertToJson)
+    //    .then(data => data)
+       .then(extractCard)
+       .then(drawCard)
+}
+
+// function convertToJson(r) {
+//     return r.json();
+// }
+
+function extractCard(cardObj) {
+    // console.log(cardObj)
+    return cardObj.question;
+    console.log(cardObj.answer)
+}
+
+// function that draws card to DOM
+
+function drawCard(cardText) {
+    const newCard = document.createElement('div');
+    newCard.textContent = cardText;
+
+    // append
+    outputElement.appendChild(newCard);
+}
+
+
+
+
+// main function that attaches button listener
+function main() {
+    triggerElement.addEventListener('click', getCard);
+}
+
+main();
